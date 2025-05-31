b9e518bf59e6e4371b3a65bcd3156c3ddb5b6ab3
Agata Bielska <agatabielska027@gmail.com>
2025-05-31 21:20:20 +0200

    Check

--- deploy.R
+++ deploy.R
@@ -1,11 +1,78 @@
-# Add custom library path explicitly
-.libPaths(c(Sys.getenv("R_LIBS_USER"), .libPaths()))
+.libPaths("/usr/local/lib/R/site-library")
+cat("loading packages from:", paste("\n - ", .libPaths(), collapse = ""), "\n\n")
 
-library(rsconnect)
-rsconnect::setAccountInfo(
-  name = Sys.getenv("SHINYAPPS_NAME"),
-  token = Sys.getenv("SHINYAPPS_TOKEN"),
-  secret = Sys.getenv("SHINYAPPS_SECRET")
+# use renv to detect and install required packages.
+if (file.exists("renv.lock")) {
+  renv::restore(prompt = FALSE)
+} else {
+  renv::hydrate()
+}
+
+# set up some helper functions for fetching environment variables
+defined <- function(name) {
+  !is.null(Sys.getenv(name)) && Sys.getenv(name) != ""
+}
+required <- function(name) {
+  if (!defined(name)) {
+    stop("!!! input or environment variable '", name, "' not set")
+  }
+  Sys.getenv(name)
+}
+optional <- function(name) {
+  if (!defined(name)) {
+    return(NULL)
+  }
+  Sys.getenv(name)
+}
+
+# resolve app dir
+# Note that we are likely already executing from the app dir, as
+# github sets the working directory to the workspace path on starting
+# the docker image.
+appDir <- ifelse(
+  defined("INPUT_APPDIR"),
+  required("INPUT_APPDIR"),
+  required("GITHUB_WORKSPACE")
 )
 
-rsconnect::deployApp(appDir = ".")
+# required inputs
+appName <- required("INPUT_APPNAME")
+accountName <- required("INPUT_ACCOUNTNAME")
+accountToken <- required("INPUT_ACCOUNTTOKEN")
+accountSecret <- required("INPUT_ACCOUNTSECRET")
+
+# optional inputs
+appFiles <- optional("INPUT_APPFILES")
+appFileManifest <- optional("INPUT_APPFILEMANIFEST")
+appTitle <- optional("INPUT_APPTITLE")
+logLevel <- optional("INPUT_LOGLEVEL")
+forceUpdate <- optional("INPUT_FORCEUPDATE")
+githubPackages <- optional("INPUT_GITHUBPACKAGES")
+
+# process appFiles
+if (!is.null(appFiles)) {
+  appFiles <- unlist(strsplit(appFiles, ",", TRUE))
+}
+
+# install packages from github
+if (!is.null(githubPackages)) {
+  githubToken <- required("INPUT_GITHUBTOKEN")
+  githubPackages <- unlist(strsplit(githubPackages, ",", TRUE))
+  devtools::install_github(githubPackages, auth_token = githubToken)
+}
+
+# set up account
+cat("checking account info...")
+rsconnect::setAccountInfo(accountName, accountToken, accountSecret)
+cat(" [OK]\n")
+
+# deploy application
+rsconnect::deployApp(
+  appDir = appDir,
+  appFiles = appFiles,
+  appFileManifest = appFileManifest,
+  appName = appName,
+  appTitle = appTitle,
+  account = accountName,
+  forceUpdate = forceUpdate
+)
