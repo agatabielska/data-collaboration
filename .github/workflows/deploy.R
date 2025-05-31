304355da7cfc3be051082efae8963db4ef84d09d
Agata Bielska <agatabielska027@gmail.com>
2025-05-31 20:36:12 +0200

    Check 7

--- .github/workflows/deploy.R
+++
@@ -1,7 +0,0 @@
-rsconnect::setAccountInfo(
-  name = Sys.getenv("SHINYAPPS_NAME"),
-  token = Sys.getenv("SHINYAPPS_TOKEN"),
-  secret = Sys.getenv("SHINYAPPS_SECRET")
-)
-
-rsconnect::deployApp(appDir = ".", appName = "your-app-name")
